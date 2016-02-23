67d5d158a7d2520804c73ec3190c7048783715a9
Thomas Schlumberger <thomas@b13.de>
2016-02-23 16:21:16 +0100

    [TASK] Fix dependencies of providers Our custom provider needs to be called before the core pageTsConfig provider as the core provider unsets the type key.

--- ext_localconf.php
+++ ext_localconf.php
@@ -24,6 +24,7 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Backend\\Form\\Contai
 
 if (\TYPO3\CMS\Core\Utility\GeneralUtility::compat_version('7.3')) {
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][\B13\Pagetsconfig\Provider\PageTsConfigForeignTableProvider::class] = array(
-		\TYPO3\CMS\Backend\Form\FormDataProvider\PageTsConfigMerged::class
+		\TYPO3\CMS\Backend\Form\FormDataProvider\PageTsConfig::class
 	);
+	$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][\TYPO3\CMS\Backend\Form\FormDataProvider\PageTsConfigMerged::class]['depends'][] = \B13\Pagetsconfig\Provider\PageTsConfigForeignTableProvider::class;
 }
