f1dc74f7805045003e7985b556377aee4bd12230
David Steeb <david.steeb@b13.com>
2022-10-28 17:42:38 +0200

    [TASK] Update requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '2.0.0-dev',
 	'constraints' => [
 		'depends' => [
-            'typo3' => '11.0.0-11.99.99',
+            'typo3' => '10.0.0-11.99.99',
         ],
     ],
 ];
