b2811ccabfe2b247f4225fb92485134a55988842
David Steeb <david.steeb@b13.com>
2022-10-28 17:36:57 +0200

    [TASK] Remove file no longer needed with TYPO3 v11

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,11 +9,4 @@ defined('TYPO3_MODE') or die();
             \TYPO3\CMS\Backend\Form\FormDataProvider\TcaTypesShowitem::class,
         ],
     ];
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][B13\Pagetsconfig\Backend\Form\FormDataProvider\TcaColumnsProcessFieldDescriptions::class] = [
-        'depends' => [
-            \TYPO3\CMS\Backend\Form\FormDataProvider\TcaTypesShowitem::class,
-            \TYPO3\CMS\Backend\Form\FormDataProvider\TcaColumnsProcessFieldLabels::class,
-            \B13\EngelhardMaster\Backend\Form\FormDataProvider\PageTsConfigFix::class
-        ],
-    ];
 })();
