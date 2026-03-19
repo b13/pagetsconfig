bddf0ef25c58b258d0f53e9f2018b6decf495f64
Achim Fritz <af@achimfritz.de>
2026-03-19 16:49:02 +0100

    [TASK] allow installation on TYPO3 v14

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,10 +3,20 @@
 defined('TYPO3') or die();
 
 (function () {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][B13\Pagetsconfig\Backend\Form\FormDataProvider\PageTsConfigFix::class] = [
-        'depends' => [
-            \TYPO3\CMS\Backend\Form\FormDataProvider\PageTsConfig::class,
-            \TYPO3\CMS\Backend\Form\FormDataProvider\TcaTypesShowitem::class,
-        ],
-    ];
+    if ((new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() < 14) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][B13\Pagetsconfig\Backend\Form\FormDataProvider\PageTsConfigFix::class] = [
+            'depends' => [
+                \TYPO3\CMS\Backend\Form\FormDataProvider\PageTsConfig::class,
+                \TYPO3\CMS\Backend\Form\FormDataProvider\TcaTypesShowitem::class,
+            ],
+        ];
+    } else {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][B13\Pagetsconfig\Backend\Form\FormDataProvider\PageTsConfigFix::class] = [
+            'depends' => [
+                \TYPO3\CMS\Backend\Form\FormDataProvider\PageTsConfig::class,
+                \TYPO3\CMS\Backend\Form\FormDataProvider\TcaColumnsProcessShowitem::class,
+            ],
+        ];
+    }
+
 })();
